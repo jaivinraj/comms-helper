1c1145e6df082b88e52c15da3e53f185b45cd60f
Jaivin Raj <jaivinraj@Jaivins-MBP.lan>
2022-05-24 13:46:45 +0100

    Add initial time series to dashboard

---
+++ src/comms_helper/dashboard/components/time_series.py
@@ -0,0 +1,31 @@
+from dash import html, dcc
+
+from __init__ import dashapp
+
+import pandas as pd
+import plotly.express as px
+from dash.dependencies import Input, Output, State
+from comms_helper.analysis.time_series import get_tweet_count
+
+
+time_series_layout = html.Div(
+    [
+        dcc.Graph(
+            id="time-series",
+        )
+    ],
+    id="time-series-layout",
+)
+
+
+@dashapp.callback(
+    Output("time-series", "figure"),
+    Input("data", "children"),
+)
+def update_time_series(json_data):
+    dff = pd.read_json(json_data, orient="split")
+    tweet_count = get_tweet_count(dff, "W")
+    fig = px.line(
+        tweet_count.to_frame("n_tweets").reset_index(), x="date", y="n_tweets"
+    )
+    return fig
--- src/comms_helper/dashboard/main.py
+++ src/comms_helper/dashboard/main.py
@@ -1,5 +1,6 @@
 """Script to run the Dash app"""
 
+from time import time
 from __init__ import dashapp
 from dash import Dash
 from dash import html
@@ -7,6 +8,7 @@ import pandas as pd
 from comms_helper.dashboard.components.hidden import hidden_layout
 from comms_helper.dashboard.components.wordcloud import wordcloud_layout
 from comms_helper.dashboard.components.search_selection import search_selection_layout
+from comms_helper.dashboard.components.time_series import time_series_layout
 
 dashapp.layout = html.Div(
     [
@@ -14,6 +16,7 @@ dashapp.layout = html.Div(
         search_selection_layout,
         wordcloud_layout,
         hidden_layout,
+        time_series_layout,
     ]
 )
 
