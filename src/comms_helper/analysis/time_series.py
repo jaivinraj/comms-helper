1ca06c630c03d5103682bdfd9d51fafa39208c9e
Jaivin Raj <jaivinraj@Jaivins-MBP.lan>
2022-05-25 09:24:22 +0100

    Add time series analysis module

---
+++ src/comms_helper/analysis/time_series.py
@@ -0,0 +1,12 @@
+resample_rule = "W"
+rolling_window = 4
+
+RULE_MAP = {"W": "Weekly", "M": "Monthly"}
+
+
+def get_tweet_count(df, resample_rule="W"):
+    return df.set_index("date").resample(resample_rule).count()["content"]
+
+
+def tweet_count_to_rolling(tweet_count, rolling_window=4):
+    return tweet_count.rolling(rolling_window).mean()
